5e1021e8965b9dc80a6f2614453bee844027f81f
David John <davidajohn@gmail.com>
2020-12-05 22:32:46 +0000

    Added EF packages and set up DbContext

---
+++ BookwormsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using BookwormsAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookwormsAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
