dfe3de891b09ff328f3dade76ef5a2564222429a
David John <davidajohn@gmail.com>
2021-02-06 17:09:15 +0000

    Added identity classes to the API, plus basic user entities

---
+++ BookwormsAPI/Entities/Identity/Address.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookwormsAPI.Entities.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+        [Required]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
