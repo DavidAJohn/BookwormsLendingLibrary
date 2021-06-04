d2dee038b5ac449f0bf05e35150810e147d9e6fa
David John <davidajohn@gmail.com>
2021-06-04 15:20:42 +0100

    Created new site status method in API admin controller

--- BookwormsAPI/Controllers/AdminController.cs
+++ BookwormsAPI/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 using System.Linq;
 using System.Threading.Tasks;
+using BookwormsAPI.Contracts;
+using BookwormsAPI.Entities.Borrowing;
 using BookwormsAPI.Entities.Identity;
+using BookwormsAPI.Specifications;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -11,8 +14,14 @@ namespace BookwormsAPI.Controllers
     public class AdminController : BaseApiController
     {
         private readonly UserManager<AppUser> _userManager;
-        public AdminController(UserManager<AppUser> userManager)
+        private readonly IBookRepository _bookRepository;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IRequestService _requestService;
+        public AdminController(UserManager<AppUser> userManager, IBookRepository bookRepository, IAuthorRepository authorRepository, IRequestService requestService)
         {
+            _requestService = requestService;
+            _authorRepository = authorRepository;
+            _bookRepository = bookRepository;
             _userManager = userManager;
         }
 
@@ -26,6 +35,27 @@ namespace BookwormsAPI.Controllers
                 .ToListAsync();
 
             return Ok(users);
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("status")]
+        public async Task<ActionResult> GetSiteStatus()
+        {
+            var bookSpec = new BooksWithFiltersForCountSpecification(new BookSpecificationParams{});
+            var bookTotal = await _bookRepository.CountAsync(bookSpec);
+
+            var authorSpec = new AuthorsWithFiltersForCountSpecification(new AuthorSpecificationParams{});
+            var authorTotal = await _authorRepository.CountAsync(authorSpec);
+
+            var requestsOutstanding = await _requestService.GetRequestsByStatusAsync(RequestStatus.Sent);
+            var requestsOverdue = await _requestService.GetRequestsOverdueAsync();
+            
+            return Ok(new{
+                BookTotal = bookTotal,
+                AuthorTotal = authorTotal,
+                RequestsOutstanding = requestsOutstanding.Count(),
+                RequestsOverdue = requestsOverdue.Count()
+            });
         }
     }
 }
