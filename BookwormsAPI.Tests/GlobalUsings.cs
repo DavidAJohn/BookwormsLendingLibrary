fd02852ca4a5f1f21ad2c4c8739c79e96727016b
David John <davidajohn@gmail.com>
2023-09-25 19:37:04 +0100

    Added a global usings file to the API test project

---
+++ BookwormsAPI.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Xunit;
+global using FluentAssertions;
+global using NSubstitute;
+global using NSubstitute.ReturnsExtensions;
+global using BookwormsAPI.Contracts;
+global using BookwormsAPI.Controllers;
+global using BookwormsAPI.DTOs;
+global using BookwormsAPI.Specifications;
