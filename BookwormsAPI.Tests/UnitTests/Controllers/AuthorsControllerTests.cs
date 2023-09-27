8180e6935685adc02f07f91d7274881af93d8de2
David John <davidajohn@gmail.com>
2023-09-27 14:04:15 +0100

    Updated the AuthorsController unit tests

--- BookwormsAPI.Tests/UnitTests/Controllers/AuthorsControllerTests.cs
+++ BookwormsAPI.Tests/UnitTests/Controllers/AuthorsControllerTests.cs
@@ -1,37 +1,32 @@
 ﻿using AutoMapper;
-using BookwormsAPI.Contracts;
-using BookwormsAPI.Controllers;
-using BookwormsAPI.DTOs;
-using BookwormsAPI.Specifications;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace BookwormsAPI.Tests.UnitTests.Controllers
 {
-    public class AuthorsControllerTests
+    public class AuthorsControllerTests : TestBase
     {
-        private readonly Mock<IAuthorRepository> _authorRepositoryMock;
-        private readonly Mock<IMapper> _mapperMock;
-        private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock;
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly DefaultHttpContext _context;
+        private readonly IMapper _mapper;
+        private AuthorsController _sut;
 
         public AuthorsControllerTests()
         {
-            _authorRepositoryMock = new Mock<IAuthorRepository>();
-            _mapperMock = new Mock<IMapper>();
-            _httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            _authorRepository = Substitute.For<IAuthorRepository>();
+            _context = new DefaultHttpContext();
+            _httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            _httpContextAccessor.HttpContext = _context;
+            _mapper = BuildMap();
+
+            _sut = new AuthorsController(_authorRepository, _mapper, _httpContextAccessor);
         }
 
         [Fact]
-        public async Task GetAuthors_ReturnsOkResult()
+        public async Task GetAuthors_ReturnsListOfAuthorDTOs_WhenCalled()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
-
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
             var authorParams = new AuthorSpecificationParams()
             {
                 PageIndex = 1,
@@ -39,120 +34,232 @@ namespace BookwormsAPI.Tests.UnitTests.Controllers
             };
 
             // Act
-            var response = await controller.GetAuthors(authorParams);
+            var result = (OkObjectResult)await _sut.GetAuthors(authorParams);
 
             // Assert
-            Assert.IsType<OkObjectResult>(response.Result);
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
         }
 
         [Fact]
-        public async Task GetAuthorById_Id_NonExistent_ReturnsNotFound()
+        public async Task GetAuthorById_ReturnsAuthorDTO_WhenIdExists()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            var author = new Author
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Test"
+            };
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            _authorRepository
+                .GetEntityWithSpec(Arg.Any<AuthorsWithBooksSpecification>())
+                .Returns(author);
 
             // Act
-            var response = await controller.GetAuthorById(9999);
+            var result = (OkObjectResult)await _sut.GetAuthorById(1);
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
         }
 
         [Fact]
-        public async Task CreateAuthor_WhenInputIsNull_ReturnsBadRequest()
+        public async Task GetAuthorById_ReturnsNotFound_WhenIdDoesNotExist()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            _authorRepository
+                .GetEntityWithSpec(Arg.Any<AuthorsWithBooksSpecification>())
+                .ReturnsNull();
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            // Act
+            var result = (NotFoundObjectResult)await _sut.GetAuthorById(int.MaxValue);
 
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task CreateAuthor_ReturnsCreatedResponse_WhenInputIsValid()
+        {
+            // Arrange
+            var authorDTO = new AuthorCreateDTO
+            {
+                FirstName = "John",
+                LastName = "Test"
+            };
+
+            var author = CreateFakeAuthor("John", "Test");
+
+            _authorRepository
+                .Create(Arg.Any<Author>())
+                .Returns(author);
+
+            // Act
+            var result = (CreatedAtRouteResult)await _sut.CreateAuthor(authorDTO);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status201Created);
+            result.RouteName.Should().Be("GetAuthorById");
+            result.Value.Should().BeOfType<AuthorDTO>();
+        }
+
+        [Fact]
+        public async Task CreateAuthor_ReturnsBadRequest_WhenInputIsInvalid()
+        {
+            // Arrange
+            var authorDTO = new AuthorCreateDTO
+            {
+                FirstName = "John",
+                // LastName is null
+            };
+
+            // Act
+            var result = (BadRequestObjectResult)await _sut.CreateAuthor(authorDTO);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public async Task CreateAuthor_ReturnsBadRequest_WhenInputIsNull()
+        {
+            // Arrange
             AuthorCreateDTO authorDTO = null;
 
             // Act
-            var response = await controller.CreateAuthor(authorDTO);
+            var result = (BadRequestObjectResult)await _sut.CreateAuthor(authorDTO);
 
             // Assert
-            Assert.IsType<BadRequestObjectResult>(response.Result);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
 
         [Fact]
-        public async Task DeleteAuthor_ReturnsActionResult()
+        public async Task CreateAuthor_ReturnsBadRequest_WhenAuthorCouldNotBeCreated()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            var authorDTO = new AuthorCreateDTO
+            {
+                FirstName = "John",
+                LastName = "Test"
+            };
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            _authorRepository
+                .Create(Arg.Any<Author>())
+                .ReturnsNull();
 
             // Act
-            var response = await controller.DeleteAuthor(1);
+            var result = (BadRequestObjectResult)await _sut.CreateAuthor(authorDTO);
 
             // Assert
-            Assert.IsAssignableFrom<ActionResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
 
         [Fact]
-        public async Task DeleteAuthor_Id_NonExistent_ReturnsNotFound()
+        public async Task DeleteAuthor_ReturnsNoContent_WhenAuthorIsDeleted()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            var author = CreateFakeAuthor("John", "Test");
+            _authorRepository.GetByIdAsync(author.Id).Returns(author);
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            _authorRepository.Delete(author).Returns(true);
 
             // Act
-            var response = await controller.DeleteAuthor(9999);
+            var result = (NoContentResult)await _sut.DeleteAuthor(author.Id);
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status204NoContent);
         }
 
         [Fact]
-        public async Task UpdateAuthor_ReturnsActionResult()
+        public async Task DeleteAuthor_ReturnsNotFound_WhenIdDoesNotExist()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            _authorRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNull();
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            // Act
+            var result = (NotFoundObjectResult)await _sut.DeleteAuthor(int.MaxValue);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task DeleteAuthor_ReturnsBadRequest_WhenAuthorIsNotDeleted()
+        {
+            // Arrange
+            var author = CreateFakeAuthor("John", "Test");
+            _authorRepository.GetByIdAsync(author.Id).Returns(author);
+
+            _authorRepository.Delete(Arg.Any<Author>()).Returns(false);
+
+            // Act
+            var result = (BadRequestObjectResult)await _sut.DeleteAuthor(author.Id);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public async Task UpdateAuthor_ReturnsNoContent_WhenAuthorIsUpdated()
+        {
+            // Arrange
+            var author = CreateFakeAuthor("John", "Test");
+
+            _authorRepository.GetByIdAsync(author.Id).Returns(author);
 
             var authorDTO = new AuthorUpdateDTO
             {
                 FirstName = "John",
-                LastName = "White"
+                LastName = "Updated"
             };
 
+            _authorRepository.Update(Arg.Any<Author>()).Returns(true);
+
             // Act
-            var response = await controller.UpdateAuthor(1, authorDTO);
+            var result = (NoContentResult)await _sut.UpdateAuthor(author.Id, authorDTO);
 
             // Assert
-            Assert.IsAssignableFrom<ActionResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status204NoContent);
         }
 
         [Fact]
-        public async Task UpdateAuthor_Id_NonExistent_ReturnsNotFound()
+        public async Task UpdateAuthor_ReturnsNotFound_WhenIdDoesNotExist()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
-            _httpContextAccessorMock.Setup(_ => _.HttpContext).Returns(httpContext);
+            var authorDTO = new AuthorUpdateDTO
+            {
+                FirstName = "John",
+                LastName = "Test"
+            };
 
-            var controller = new AuthorsController(_authorRepositoryMock.Object, _mapperMock.Object, _httpContextAccessorMock.Object);
+            _authorRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNull();
+
+            // Act
+            var result = (NotFoundObjectResult)await _sut.UpdateAuthor(int.MaxValue, authorDTO);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task UpdateAuthor_ReturnsBadRequest_WhenAuthorIsNotUpdated()
+        {
+            // Arrange
+            var author = CreateFakeAuthor("John", "Test");
+            _authorRepository.GetByIdAsync(author.Id).Returns(author);
 
             var authorDTO = new AuthorUpdateDTO
             {
                 FirstName = "John",
-                LastName = "White"
+                LastName = "Updated"
             };
 
+            _authorRepository.Update(Arg.Any<Author>()).Returns(false);
+
             // Act
-            var response = await controller.UpdateAuthor(1, authorDTO);
+            var result = (BadRequestObjectResult)await _sut.UpdateAuthor(author.Id, authorDTO);
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
     }
 }
