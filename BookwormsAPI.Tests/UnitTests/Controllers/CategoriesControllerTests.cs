5a2e08f1e7ae305f3bef6cae26ea97b325af82f5
David John <davidajohn@gmail.com>
2023-09-28 22:44:06 +0100

    Updated the CategoriesController unit tests

--- BookwormsAPI.Tests/UnitTests/Controllers/CategoriesControllerTests.cs
+++ BookwormsAPI.Tests/UnitTests/Controllers/CategoriesControllerTests.cs
@@ -1,152 +1,274 @@
-using BookwormsAPI.Contracts;
-using BookwormsAPI.Controllers;
-using BookwormsAPI.DTOs;
-using BookwormsAPI.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
-using System;
-using System.Threading.Tasks;
-using Xunit;
 
 namespace BookwormsAPI.Tests.UnitTests.Controllers
 {
     public class CategoriesControllerTests : TestBase
     {
-        [Fact]
-        public async Task GetCategories_ReturnsOkResult()
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        private CategoriesController _sut;
+
+        public CategoriesControllerTests()
         {
-            // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
+            _mapper = BuildMap();
+            _categoryRepository = Substitute.For<ICategoryRepository>();
 
-            // Act
-            var response = await controller.GetCategories();
-
-            // Assert
-            Assert.IsType<OkObjectResult>(response.Result);
+            _sut = new CategoriesController(_categoryRepository, _mapper);
         }
 
         [Fact]
-        public async Task GetCategoryById_Id_NonExistent_ReturnsNotFound()
+        public async Task GetCategories_ReturnsListOfCategories_WhenCalled()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
+            _categoryRepository
+                .ListAsync(Arg.Any<CategoriesOrderedByNameSpecification>())
+                .Returns(new List<Category>());
 
             // Act
-            var response = await controller.GetCategoryById(9999);
+            var result = (OkObjectResult)await _sut.GetCategories();
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.Value.Should().BeOfType<List<Category>>();
         }
 
         [Fact]
-        public async Task CreateCategory_WhenCalled_ReturnsActionResultOfCategoryDTO()
+        public async Task GetCategoryById_ReturnsCategory_WhenIdExists()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
+            var id = 1;
 
+            Category category = new()
+            {
+                Id = id,
+                Name = "Category 1"
+            };
+
+            _categoryRepository
+                .GetEntityWithSpec(Arg.Any<CategoriesOrderedByNameSpecification>())
+                .Returns(category);
+
+            // Act
+            var result = (OkObjectResult)await _sut.GetCategoryById(id);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status200OK);
+            result.Value.Should().BeOfType<Category>();
+            result.Value.Should().BeEquivalentTo(category);
+        }
+
+        [Fact]
+        public async Task GetCategoryById_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            // Arrange
+            _categoryRepository
+                .GetEntityWithSpec(Arg.Any<CategoriesOrderedByNameSpecification>())
+                .ReturnsNull();
+
+            // Act
+            var result = (NotFoundObjectResult)await _sut.GetCategoryById(int.MaxValue);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task CreateCategory_ReturnsCategoryDTO_WhenCategoryIsCreated()
+        {
+            // Arrange
             var catDTO = new CategoryCreateDTO
             {
                 Name = "Category 1"
             };
 
+            var category = new Category
+            {
+                Id = 1,
+                Name = "Category 1"
+            };
+
+            _categoryRepository.Create(Arg.Any<Category>()).Returns(category);
+
             // Act
-            var response = await controller.CreateCategory(catDTO);
+            var result = (CreatedAtRouteResult)await _sut.CreateCategory(catDTO);
 
             // Assert
-            Assert.IsType<ActionResult<CategoryDTO>>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status201Created);
+            result.RouteName.Should().Be("GetCategoryById");
+            result.Value.Should().BeOfType<CategoryDTO>();
         }
 
         [Fact]
-        public async Task CreateCategory_WhenInputIsNull_ReturnsBadRequest()
+        public async Task CreateCategory_ReturnsBadRequest_WhenInputIsNull()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
-
             CategoryCreateDTO catDTO = null;
 
             // Act
-            var response = await controller.CreateCategory(catDTO);
+            var result = (BadRequestObjectResult)await _sut.CreateCategory(catDTO);
 
             // Assert
-            Assert.IsType<BadRequestObjectResult>(response.Result);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
 
         [Fact]
-        public async Task DeleteCategory_ReturnsActionResult()
+        public async Task CreateCategory_ReturnsBadRequest_WhenInputIsInvalid()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
+            var catDTO = new CategoryCreateDTO
+            {
+                Name = null
+            };
 
             // Act
-            var response = await controller.DeleteCategory(1);
+            var result = (BadRequestObjectResult)await _sut.CreateCategory(catDTO);
 
             // Assert
-            Assert.IsAssignableFrom<ActionResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
 
         [Fact]
-        public async Task DeleteCategory_Id_NonExistent_ReturnsNotFound()
+        public async Task CreateCategory_ReturnsBadRequest_WhenCategoryCouldNotBeCreated()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
-
-            // Act
-            var response = await controller.DeleteCategory(9999);
-
-            // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
-        }
-
-        [Fact]
-        public async Task UpdateCategory_ReturnsActionResult()
-        {
-            // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
-
             var catDTO = new CategoryCreateDTO
             {
                 Name = "Category 1"
             };
 
+            _categoryRepository.Create(Arg.Any<Category>()).ReturnsNull();
+
             // Act
-            var response = await controller.UpdateCategory(1, catDTO);
+            var result = (BadRequestObjectResult)await _sut.CreateCategory(catDTO);
 
             // Assert
-            Assert.IsAssignableFrom<ActionResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
 
         [Fact]
-        public async Task UpdateCategory_Id_NonExistent_ReturnsNotFound()
+        public async Task DeleteCategory_ReturnsNoContent_WhenCategoryIsDeleted()
         {
             // Arrange
-            var mapper = BuildMap();
-            var mockRepo = new Mock<ICategoryRepository>();
-            var controller = new CategoriesController(mockRepo.Object, mapper);
+            var id = 1;
 
+            Category category = new()
+            {
+                Id = id,
+                Name = "Category 1"
+            };
+
+            _categoryRepository.GetByIdAsync(Arg.Any<int>()).Returns(category);
+            _categoryRepository.Delete(Arg.Any<Category>()).Returns(true);
+
+            // Act
+            var result = (NoContentResult)await _sut.DeleteCategory(id);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            // Arrange
+            _categoryRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNull();
+
+            // Act
+            var result = (NotFoundObjectResult)await _sut.DeleteCategory(int.MinValue);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task DeleteCategory_ReturnsBadRequest_WhenCategoryIsNotDeleted()
+        {
+            // Arrange
+            Category category = new()
+            {
+                Id = 1,
+                Name = "Category 1"
+            };
+
+            _categoryRepository.GetByIdAsync(category.Id).Returns(category);
+
+            _categoryRepository.Delete(Arg.Any<Category>()).Returns(false);
+
+            // Act
+            var result = (BadRequestObjectResult)await _sut.DeleteCategory(category.Id);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_ReturnsNoContent_WhenCategoryIsUpdated()
+        {
+            // Arrange
             var catDTO = new CategoryCreateDTO
             {
                 Name = "Category 1"
             };
 
+            var category = new Category
+            {
+                Id = 1,
+                Name = "Category 1"
+            };
+
+            _categoryRepository.GetByIdAsync(category.Id).Returns(category);
+            _categoryRepository.Update(Arg.Any<Category>()).Returns(true);
+
             // Act
-            var response = await controller.UpdateCategory(9999, catDTO);
+            var result = (NoContentResult)await _sut.UpdateCategory(category.Id, catDTO);
 
             // Assert
-            Assert.IsType<NotFoundObjectResult>(response);
+            result.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            // Arrange
+            var catDTO = new CategoryCreateDTO
+            {
+                Name = "Category 1"
+            };
+
+            _categoryRepository.GetByIdAsync(Arg.Any<int>()).ReturnsNull();
+
+            // Act
+            var result = (NotFoundObjectResult)await _sut.UpdateCategory(int.MaxValue, catDTO);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public async Task UpdateCategory_ReturnsBadRequest_WhenCategoryWasNotUpdated()
+        {
+            // Arrange
+            var catDTO = new CategoryCreateDTO
+            {
+                Name = "Category 1"
+            };
+
+            var category = new Category
+            {
+                Id = 1,
+                Name = "Category 1"
+            };
+
+            _categoryRepository.GetByIdAsync(category.Id).Returns(category);
+            _categoryRepository.Update(Arg.Any<Category>()).Returns(false);
+
+            // Act
+            var result = (BadRequestObjectResult)await _sut.UpdateCategory(category.Id, catDTO);
+
+            // Assert
+            result.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
         }
     }
 }
