f1b3515c82d00ccd913b7557858f349711fbfa33
David John <davidajohn@gmail.com>
2024-06-14 20:30:16 +0100

    Added unit tests for the API project's token service

---
+++ BookwormsAPI.Tests/UnitTests/Services/TokenServiceTests.cs
@@ -0,0 +1,62 @@
+﻿using BookwormsAPI.Entities.Identity;
+using BookwormsAPI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BookwormsAPI.Tests.UnitTests.Services;
+
+public class TokenServiceTests
+{
+    private readonly TokenService _tokenService;
+    private readonly IConfiguration _config;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly AppUser _user;
+
+    public TokenServiceTests()
+    {
+        var inMemoryConfig = new Dictionary<string, string> {
+            {"Token:Key", "£MockJwtKeyMockJwtKeyMockJwtKey$_£MockJwtKeyMockJwtKeyMockJwtKey$"},
+            {"Token:Issuer", "http://testissuer"}
+        };
+
+        _config = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryConfig)
+            .Build();
+
+        _user = new AppUser { Email = "test@test.com", DisplayName = "Test User" };
+
+        var store = Substitute.For<IUserStore<AppUser>>();
+        _userManager = Substitute.For<UserManager<AppUser>>(store, null, null, null, null, null, null, null, null);
+
+        _tokenService = new TokenService(_config, _userManager);
+    }
+
+    [Fact]
+    public async Task CreateToken_ShouldReturnToken_WhenInvoked()
+    {
+        // Act
+        var token = await _tokenService.CreateToken(_user);
+
+        // Assert
+        token.Should().BeOfType<string>();
+        token.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task CreateToken_ShouldReturnTokenWithClaims_WhenInvoked()
+    {
+        // Act
+        var token = await _tokenService.CreateToken(_user);
+
+        // Assert
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+        securityToken.Should().NotBeNull();
+        securityToken.Issuer.Should().Be(_config["Token:Issuer"]);
+        securityToken.Claims.Should().NotBeEmpty();
+        securityToken.Claims.Should().Contain(claim => claim.Type == JwtRegisteredClaimNames.Email && claim.Value == _user.Email);
+        securityToken.Claims.Should().Contain(claim => claim.Type == JwtRegisteredClaimNames.GivenName && claim.Value == _user.DisplayName);
+    }
+}
--- BookwormsAPI/Services/TokenService.cs
+++ BookwormsAPI/Services/TokenService.cs
@@ -1,56 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using BookwormsAPI.Contracts;
 using BookwormsAPI.Entities.Identity;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-namespace BookwormsAPI.Services
+namespace BookwormsAPI.Services;
+
+public class TokenService : ITokenService
 {
-    public class TokenService : ITokenService
+    private readonly IConfiguration _config;
+    private readonly SymmetricSecurityKey _key;
+    private readonly UserManager<AppUser> _userManager;
+    public TokenService(IConfiguration config, UserManager<AppUser> userManager)
     {
-        private readonly IConfiguration _config;
-        private readonly SymmetricSecurityKey _key;
-        private readonly UserManager<AppUser> _userManager;
-        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
+        _userManager = userManager;
+        _config = config;
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
+    }
+
+    public async Task<string> CreateToken(AppUser user)
+    {
+        var claims = new List<Claim>
         {
-            _userManager = userManager;
-            _config = config;
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Token:Key"]));
-        }
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.GivenName, user.DisplayName)
+        };
 
-        public async Task<string> CreateToken(AppUser user)
+        var roles = await _userManager.GetRolesAsync(user);
+
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim(JwtRegisteredClaimNames.GivenName, user.DisplayName)
-            };
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds,
+            Issuer = _config["Token:Issuer"]
+        };
 
-            var roles = await _userManager.GetRolesAsync(user);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
 
-            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
-
-            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddDays(7),
-                SigningCredentials = creds,
-                Issuer = _config["Token:Issuer"]
-            };
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
-        }
+        return tokenHandler.WriteToken(token);
     }
 }
